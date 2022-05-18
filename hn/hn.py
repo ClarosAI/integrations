11fb1f07003525ab6d402afbc62d40a71dc1d478
Anish Thite <anishthite@gmail.com>
2022-05-18 18:01:24 -0400

    update for addsources

--- hn/hn.py
+++ hn/hn.py
@@ -3,13 +3,14 @@ import requests
 import time
 
 #Get your API Key from us (email anish)
-API_KEY = ''
+API_KEY = 'hello'
 
 # Check README -- generated from external library
 with open('links.json', 'r') as f:
     links = json.load(f)
 links = links['links']
 
+urls = []
 for d in links:
     
     #index comment
@@ -33,20 +34,22 @@ for d in links:
 
     #index actual
     if d['url'] != d['comments_url']:
-        data = {
-            'link': d['url'],
-            'collection': 'anish@diva.so'
-        }
-        response = requests.post('https://mk1.diva.so:4242/addLink',
-            json=data,
-            headers={
-                'Authorization': f"Bearer {API_KEY}",
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
-            }
-        )
-        print(d['url'])
-        print(response.status_code)
-        print(response.text)
+        urls.append(d['url'])
 
+data = {
+    'docs': urls,
+    'collection': 'anish@diva.so',
+    'type': 'link'
+}
+response = requests.post('https://mk1.diva.so:4242/addSources',
+    json=data,
+    headers={
+        'Authorization': f"Bearer {API_KEY}",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    }
+)
+print(urls)
+print(response.status_code)
+print(response.text)
 
